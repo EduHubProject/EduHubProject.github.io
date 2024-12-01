ddb7159160cd436aa2e80430a3d709c1b58d4c7a
EduHubProject <notogotshut@gmail.com>
2024-12-01 21:55:01 +0500

    Autonom Deployment

---
+++ Footer-e48d9f07.js
@@ -0,0 +1,34 @@
+import { R as React } from './bundle.js';
+
+const Footer = () => {
+  return /*#__PURE__*/React.createElement("footer", {
+    className: "footer"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "footer__container"
+  }, /*#__PURE__*/React.createElement("nav", {
+    className: "footer__menu menu-footer"
+  }, /*#__PURE__*/React.createElement("ul", {
+    className: "menu-footer__list"
+  }, /*#__PURE__*/React.createElement("li", {
+    className: "menu-footer__item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#main",
+    className: "menu-footer__link menu-footer__link_home"
+  }, "\u0413\u043B\u0430\u0432\u043D\u0430\u044F")), /*#__PURE__*/React.createElement("li", {
+    className: "menu-footer__item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#about",
+    className: "menu-footer__link menu-footer__link_about"
+  }, "\u041E \u043F\u0440\u043E\u0435\u043A\u0442\u0435")), /*#__PURE__*/React.createElement("li", {
+    className: "menu-footer__item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#contacts",
+    className: "menu-footer__link menu-footer__link_contacts"
+  }, "\u041E\u0431\u0440\u0430\u0449\u0435\u043D\u0438\u0435")))), /*#__PURE__*/React.createElement("div", {
+    className: "footer__social social"
+  }, "\u0420\u0430\u0437\u0440\u0430\u0431\u043E\u0442\u0447\u0438\u043A - \u041F\u043E\u0442\u0430\u043F\u0447\u0435\u043D\u043A\u043E \u041C\u0430\u043A\u0441\u0438\u043C | +7 (778) 546-38-99", /*#__PURE__*/React.createElement("a", {
+    href: "https://github.com/EduHubProject/EduHubProject.github.io"
+  }, "- GitHub \u0421\u0430\u0439\u0442\u0430"))));
+};
+
+export { Footer as default };
