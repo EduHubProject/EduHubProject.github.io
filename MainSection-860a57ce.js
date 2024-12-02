5487ba888d5377bf74bdad7e5baf4880c9f432ef
EduHubProject <notogotshut@gmail.com>
2024-12-02 11:23:46 +0500

    Updates

---
+++ MainSection-860a57ce.js
@@ -0,0 +1,265 @@
+import { R as React, L as Link, r as reactExports, Q, B } from './bundle.js';
+
+var mainImage = "18f433af20f28734.webp";
+
+const MainSection = /*#__PURE__*/React.memo(() => /*#__PURE__*/React.createElement("section", {
+  className: "page__main main",
+  id: "main"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "main__container"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "main__content"
+}, /*#__PURE__*/React.createElement("h1", {
+  className: "main__title"
+}, "\u0417\u0410\u0414\u0410\u0427\u0418 ", /*#__PURE__*/React.createElement("span", null, "\u0416\u0418\u0417\u041D\u0418")), /*#__PURE__*/React.createElement("div", {
+  className: "main__buttons"
+}, /*#__PURE__*/React.createElement(Link, {
+  to: "/",
+  className: "main__hire-button button"
+}, "\u0413\u043E\u043B\u043E\u0432\u043E\u043B\u043E\u043C\u043A\u0438"), /*#__PURE__*/React.createElement(Link, {
+  to: "/",
+  className: "main__hire-button button button_dark"
+}, /*#__PURE__*/React.createElement("span", null, "\u0421\u0432\u043E\u0434\u043A\u0430 \u043F\u043E \u0430\u043D\u043A\u0435\u0442\u0435")))), /*#__PURE__*/React.createElement("div", {
+  className: "main__image"
+}, /*#__PURE__*/React.createElement("img", {
+  src: mainImage,
+  alt: "People and PC",
+  width: "517px",
+  height: "597px"
+})))));
+const AboutSection = /*#__PURE__*/React.memo(() => /*#__PURE__*/React.createElement("section", {
+  className: "page__about about",
+  id: "about"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "about__container"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "about__content"
+}, /*#__PURE__*/React.createElement("h2", {
+  className: "about__title title"
+}, "\u041E ", /*#__PURE__*/React.createElement("span", null, "\u043F\u0440\u043E\u0435\u043A\u0442\u0435")), /*#__PURE__*/React.createElement("div", {
+  className: "about__text"
+}, /*#__PURE__*/React.createElement("p", null, "\u041F\u0440\u043E\u0435\u043A\u0442 \u0441\u043E\u0437\u0434\u0430\u043D \u0441 \u0446\u0435\u043B\u044C\u044E \u043F\u043E\u043A\u0430\u0437\u0430\u0442\u044C, \u043A\u0430\u043A \u0440\u0430\u0431\u043E\u0442\u0430\u0435\u0442 \u0432\u0432\u0435\u0434\u0435\u043D\u0438\u0435 \u043D\u043E\u0432\u0448\u0435\u0441\u0442\u0432 \u0432 \u043F\u043B\u0430\u043D\u0435 \u0440\u0435\u0448\u0435\u043D\u0438\u044F \u0433\u043E\u043B\u043E\u0432\u043E\u043B\u043E\u043C\u043E\u043A \u0438 \u0437\u0430\u0434\u0430\u0447. \u041C\u044B \u0445\u043E\u0442\u0438\u043C \u043F\u043E\u043A\u0430\u0437\u0430\u0442\u044C, \u043A\u0430\u043A \u043D\u0435\u0439\u0440\u043E\u0441\u0435\u0442\u0438 \u0432 \u0441\u043A\u043E\u0440\u043E\u043C \u043F\u043B\u043E\u0442\u043D\u043E \u0432\u043E\u0439\u0434\u0443\u0442 \u0432 \u043D\u0430\u0448\u0443 \u0436\u0438\u0437\u043D\u044C, \u043A\u0430\u043A \u043E\u043D\u0438 \u043F\u043E\u043C\u043E\u0433\u0430\u044E\u0442 \u043D\u0430\u043C \u0440\u0435\u0448\u0430\u0442\u044C \u043F\u043E\u0432\u0441\u0435\u0434\u043D\u0435\u0432\u043D\u044B\u0435 \u0437\u0430\u0434\u0430\u0447\u0438, \u043A\u0430\u043A\u0438\u0435 \u0433\u043E\u043B\u043E\u0432\u043E\u043B\u043E\u043C\u043A\u0438 \u0440\u0430\u0437\u0432\u0438\u0432\u0430\u044E\u0442 \u043D\u0430\u043C \u043C\u043E\u0437\u0433 \u0438 \u0441 \u043A\u0430\u043A\u0438\u043C\u0438 \u0437\u0430\u0434\u0430\u0447\u0430\u043C\u0438 \u043C\u044B \u0432\u0441\u0442\u0440\u0435\u0447\u0430\u0435\u043C\u0441\u044F \u0432 \u043F\u043E\u0432\u0441\u0435\u0434\u043D\u0435\u0432\u043D\u043E\u0439 \u0436\u0438\u0437\u043D\u0438. \u042D\u0442\u0438\u043C \u043F\u0440\u043E\u0435\u043A\u0442\u043E\u043C \u043C\u044B \u0445\u043E\u0442\u0438\u043C \u043F\u043E\u043A\u0430\u0437\u0430\u0442\u044C \u0446\u0435\u043D\u043D\u043E\u0441\u0442\u044C \u0440\u0435\u0448\u0435\u043D\u0438\u044F \u043C\u0430\u0442\u0435\u043C\u0430\u0442\u0438\u0447\u0435\u0441\u043A\u0438\u0445 \u0433\u043E\u043B\u043E\u0432\u043E\u043B\u043E\u043C\u043E\u043A \u0438 \u0446\u0435\u043D\u043D\u043E\u0441\u0442\u044C \u043D\u0435\u0439\u0440\u043E\u0441\u0435\u0442\u0435\u0439 \u0432 \u043D\u0430\u0448\u0435\u0439 \u0436\u0438\u0437\u043D\u0438, \u043F\u0440\u043E\u0433\u043D\u043E\u0437\u0438\u0440\u043E\u0432\u0430\u043D\u0438\u0435 \u043D\u0430 \u0431\u0443\u0434\u0443\u0449\u0443\u044E \u043F\u043E\u043F\u0443\u043B\u044F\u0440\u043D\u043E\u0441\u0442\u044C \u043D\u0435\u0439\u0440\u043E\u0441\u0435\u0442\u0435\u0439 \u0441\u0440\u0435\u0434\u0438 \u043B\u044E\u0434\u0435\u0439."))), /*#__PURE__*/React.createElement("div", {
+  className: "about__image"
+}, /*#__PURE__*/React.createElement("img", {
+  src: "/assets/about/image.webp",
+  alt: "Around digital",
+  loading: "lazy",
+  width: "656px",
+  height: "597px"
+})))));
+const workItems = [{
+  type: 'digital',
+  imageSrc: '/assets/works/items/site.webp',
+  imageAlt: 'SiteDigital',
+  text: 'Сайт для новейшего подхода в обучение',
+  link: 'http://dailyfes.beget.tech/'
+}, {
+  type: 'digital',
+  imageSrc: '/assets/works/items/forum.webp',
+  imageAlt: 'Forum for Digital',
+  text: 'Тематический форум для помощи в решении головоломок',
+  link: '/'
+}, {
+  type: 'test',
+  imageSrc: '/assets/works/items/result.webp',
+  imageAlt: 'Test and answers',
+  text: 'Вопросы и результаты анкетирования',
+  link: '/'
+}, {
+  type: 'digital',
+  imageSrc: '/assets/works/items/digitalpuz.webp',
+  imageAlt: 'Digital Puzzles for practice',
+  text: 'Цифровые головоломки и решения их',
+  link: '/'
+}, {
+  type: 'life',
+  imageSrc: '/assets/works/items/puzzle.webp',
+  imageAlt: 'Real Life Puzzles',
+  text: 'Головоломки вживую',
+  link: '/'
+}, {
+  type: 'test',
+  imageSrc: '/assets/works/items/puzzleforcom.webp',
+  imageAlt: 'Commision`s puzzles',
+  text: 'Головоломки и задачи для комиссии',
+  link: '/'
+}];
+const PracticeWork = /*#__PURE__*/React.memo(() => {
+  const [activeType, setActiveType] = React.useState('');
+  const handleTypeChange = type => {
+    setActiveType(type);
+  };
+  return /*#__PURE__*/React.createElement("section", {
+    className: "page__works works",
+    id: "practice"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "works__container"
+  }, /*#__PURE__*/React.createElement("h2", {
+    className: "works__title title"
+  }, "\u041F\u0440\u0430\u043A\u0442\u0438\u0447\u0435\u0441\u043A\u0430\u044F \u0440\u0430\u0431\u043E\u0442\u0430"), /*#__PURE__*/React.createElement("div", {
+    className: "works__items items-works"
+  }, /*#__PURE__*/React.createElement("nav", {
+    className: "items-works__navigation"
+  }, /*#__PURE__*/React.createElement("button", {
+    "data-work-type": "",
+    className: `items-works__type button button_dark ${activeType === '' ? 'active' : ''}`,
+    onClick: () => handleTypeChange('')
+  }, "\u0412\u0441\u0435"), /*#__PURE__*/React.createElement("button", {
+    "data-work-type": "digital",
+    className: `items-works__type button button_dark ${activeType === 'digital' ? 'active' : ''}`,
+    onClick: () => handleTypeChange('digital')
+  }, "\u0426\u0438\u0444\u0440\u043E\u0432\u043E\u0435"), /*#__PURE__*/React.createElement("button", {
+    "data-work-type": "test",
+    className: `items-works__type button button_dark ${activeType === 'test' ? 'active' : ''}`,
+    onClick: () => handleTypeChange('test')
+  }, "\u0410\u043D\u043A\u0435\u0442\u044B"), /*#__PURE__*/React.createElement("button", {
+    "data-work-type": "life",
+    className: `items-works__type button button_dark ${activeType === 'life' ? 'active' : ''}`,
+    onClick: () => handleTypeChange('life')
+  }, "\u0416\u0438\u0432\u043E\u0435")), /*#__PURE__*/React.createElement("div", {
+    className: "items-works__body"
+  }, workItems.filter(item => item.type === activeType || activeType === '').map((item, index) => /*#__PURE__*/React.createElement("a", {
+    key: index,
+    "data-work-type": item.type,
+    href: item.link,
+    className: "items-works__item"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "item-works__image"
+  }, /*#__PURE__*/React.createElement("picture", null, /*#__PURE__*/React.createElement("source", {
+    srcSet: item.imageSrc,
+    type: "image/webp"
+  }), /*#__PURE__*/React.createElement("img", {
+    src: item.imageSrc.replace('.webp', '.jpg'),
+    alt: item.imageAlt,
+    loading: "lazy"
+  }))), item.text))))));
+});
+const ContactSection = /*#__PURE__*/React.memo(() => {
+  const [name, setName] = reactExports.useState('');
+  const [email, setEmail] = reactExports.useState('');
+  const [message, setMessage] = reactExports.useState('');
+  const [error, setError] = reactExports.useState('');
+  const handleSubmit = async event => {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    const formData = new FormData();
+    formData.append('Имя', name);
+    formData.append('Email', email);
+    formData.append('Обращение', message);
+    console.log('Отправка данных:', {
+      name,
+      email,
+      message
+    }); // Отладка
+
+    try {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxkLlixjPn2qSDzPLqq2jcrDonsh6M8PrADyJrpvs-3_dSOrfr348Agr9RM2AXryeOQ6Q/exec', {
+        method: 'POST',
+        body: formData
+      });
+
+      // Проверяем, был ли запрос успешным
+      if (!response.ok) {
+        // Если статус ответа не 2xx, выбрасываем ошибку
+        throw new Error('Сервер вернул ошибку. Статус: ' + response.status);
+      }
+      const data = await response.json(); // Получаем JSON-ответ
+      console.log('Ответ от сервера:', data); // Отладка
+
+      // Проверяем, является ли результат успешным
+      if (data.result === "Success") {
+        B.success(`Спасибо за ваше обращение, ${name}! Мы свяжемся с вами в ближайшее время.`, {
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true
+        });
+        setError('');
+        setName('');
+        setEmail('');
+        setMessage('');
+      } else if (data.error) {
+        // Если есть ошибка, выведем сообщение об ошибке
+        B.error(data.error, {
+          autoClose: 5000
+        });
+      } else {
+        // Если ответ не содержит ошибки, но не был успешным
+        B.error('Произошла ошибка при отправке формы.', {
+          autoClose: 5000
+        });
+      }
+    } catch (error) {
+      B.error('Произошла ошибка при отправке формы: ' + error.message);
+      console.error('Ошибка:', error); // Отладка
+    }
+  };
+  return /*#__PURE__*/React.createElement("section", {
+    className: "page__contacts contacts"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "contacts__container"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "contacts__content"
+  }, /*#__PURE__*/React.createElement("h2", {
+    className: "contacts__title title"
+  }, "\u0412\u044B \u043C\u043E\u0436\u0435\u0442\u0435 \u043F\u043E\u043C\u043E\u0447\u044C ", /*#__PURE__*/React.createElement("span", null, "\u043F\u0440\u043E\u0435\u043A\u0442\u0443!"))), /*#__PURE__*/React.createElement("form", {
+    onSubmit: handleSubmit,
+    autoComplete: "off",
+    className: "contacts__form form"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "form__item"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "i-1",
+    className: "form__label"
+  }, "\u0412\u0430\u0448\u0435 \u0438\u043C\u044F"), /*#__PURE__*/React.createElement("input", {
+    name: "\u0418\u043C\u044F",
+    required: true,
+    id: "i-1",
+    type: "text",
+    className: "form__input",
+    placeholder: "\u0418\u043C\u044F",
+    value: name,
+    onChange: e => setName(e.target.value)
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "form__item"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "i-2",
+    className: "form__label"
+  }, "\u0412\u0430\u0448\u0430 \u043F\u043E\u0447\u0442\u0430"), /*#__PURE__*/React.createElement("input", {
+    name: "Email",
+    required: true,
+    id: "i-2",
+    type: "email",
+    className: "form__input",
+    placeholder: "\u041F\u043E\u0447\u0442\u0430",
+    value: email,
+    onChange: e => setEmail(e.target.value)
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "form__item"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "i-3",
+    className: "form__label"
+  }, "\u0412\u0430\u0448\u0435 \u043E\u0431\u0440\u0430\u0449\u0435\u043D\u0438\u0435"), /*#__PURE__*/React.createElement("textarea", {
+    name: "\u041E\u0431\u0440\u0430\u0449\u0435\u043D\u0438\u0435",
+    required: true,
+    id: "i-3",
+    className: "form__input",
+    placeholder: "\u041E\u0431\u0440\u0430\u0449\u0435\u043D\u0438\u0435",
+    value: message,
+    onChange: e => setMessage(e.target.value)
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "form__item"
+  }, /*#__PURE__*/React.createElement("button", {
+    type: "submit",
+    className: "form__button button button_dark"
+  }, /*#__PURE__*/React.createElement("span", null, "\u041E\u0442\u043F\u0440\u0430\u0432\u0438\u0442\u044C")))), /*#__PURE__*/React.createElement(Q, null), " "));
+});
+var MainSection$1 = {
+  MainSection,
+  AboutSection,
+  PracticeWork,
+  ContactSection
+};
+
+export { MainSection$1 as default };
